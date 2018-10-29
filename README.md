b4236aa374805eb8abd05a2dfbb0e0dba03de664
Pawan Gogad <pg1099@txstate.edu>
2018-10-29 11:03:15 -0700

    More refactoring

---
+++ README.md
@@ -0,0 +1,5 @@
+docker run --name postgresql -it --rm --publish 5433:5432 --env 'PG_PASSWORD=test' --volume /Users/pawan/workspace/db-migration/data:/var/lib/postgresql --name postgres_doc sameersbn/postgresql:10
+
+docker run -it --name ps-logstash --link some-rabbit --rm --volume /Users/pawan/workspace/db-migration-python/fs/:/data/ tech-spawn-logstash:latest
+
+docker run -it --rm --hostname my-rabbit --name some-rabbit -p 8080:15672 -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password rabbitmq:3-management
--- src/migrate_large_tables.py
+++ src/migrate_large_tables.py
@@ -10,19 +10,7 @@ from common_utils import get_mapping, get_primary_key, get_type, create_value_ma
 from db_connections import source, destination, cfg, base_dir
 
 
-def create_batch_insert(destination_schema, destination_table, schema, table_name, sd, ed):
-    file = "%s/mappings/%s.json" % (base_dir, table_name)
-    mapping = get_mapping(file)
-    col_type_dest = get_type(destination_schema, destination_table)
-    pk = get_primary_key(table_name)
-    lst = list()
-    for key in mapping.keys():
-        if mapping[key]['with_comma']:
-            lst.append('"%s"' % key)
-        else:
-            lst.append(key)
-    lst.append('ROW_NUMBER() OVER(ORDER BY %s)' % pk)
-    select_str = ",".join(lst)
+def create_batch_insert(schema, table_name, col_type_dest, select_str, mapping, insert_sql, key_lst, sd, ed):
     sql_count = 'select count(*) from %s.%s where create_date >= \'%s\' and create_date <= \'%s\'' \
                 % (schema, table_name, sd, ed)
 
@@ -30,27 +18,29 @@ def create_batch_insert(destination_schema, destination_table, schema, table_nam
     cur.execute(sql_count)
     results = cur.fetchone()
     no_rows = results['count']
-    print('Total records found %s' % str(no_rows))
+    print('Total records found %d' % no_rows)
     cur.close()
 
     batch_sz = int(cfg['source']['batch'])
     itr = 1
     if no_rows > batch_sz:
         itr = int((no_rows / batch_sz) + 1)
-    print('No of itrs : %s' % str(itr))
+    print('Total number of iterations : %s' % str(itr))
 
-    sql, key_lst = create_insert_part(destination_schema, destination_table, col_type_dest, mapping)
-    table = 'select %s from %s.%s where create_date >= \'%s\' and create_date <= \'%s\'' \
-            % (select_str, schema, table_name, sd, ed)
+    select_sql = 'select %s from %s.%s where create_date >= \'%s\' and create_date <= \'%s\'' \
+                 % (select_str, schema, table_name, sd, ed)
 
     for i in range(itr):
-        task(int(i * batch_sz), batch_sz, table, mapping, key_lst, col_type_dest, sql)
+        task(i, select_sql, mapping, key_lst, col_type_dest, insert_sql)
 
 
-def task(offset, batch_sz, table, mapping, key_lst, col_type_dest, sql):
+def task(i, select_sql, mapping, key_lst, col_type_dest, insert_sql):
+    print('Iteration number %d' % (i + 1))
+    batch_sz = int(cfg['source']['batch'])
+    offset = int(i * batch_sz)
     upper_limit = offset + batch_sz
     sql_select = 'select * from (%s) x where ROW_NUMBER > %s and ROW_NUMBER <= %s' \
-                 % (table, str(offset), str(upper_limit))
+                 % (select_sql, str(offset), str(upper_limit))
     print(sql_select)
     cur = source.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(sql_select)
@@ -68,21 +58,36 @@ def task(offset, batch_sz, table, mapping, key_lst, col_type_dest, sql):
         values_lst.append(values)
 
     dest = destination.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    psycopg2.extras.execute_values(dest, sql + ' %s', values_lst, template=None, page_size=1000)
+    psycopg2.extras.execute_values(dest, insert_sql + ' %s', values_lst, template=None, page_size=10000)
     destination.commit()
     dest.close()
 
 
-def count_down(destination_schema, destination_table, schema, table_name, sd, ed):
+def count_down(dschema, dtable, schema, table_name, sd, ed):
     start_date = datetime.datetime.strptime('%s 00:00:00' % sd, '%Y-%m-%d %H:%M:%S')
     end_date = datetime.datetime.strptime('%s 23:59:59' % ed, '%Y-%m-%d %H:%M:%S')
     current = start_date
+
+    file = "%s/mappings/%s.json" % (base_dir, table_name)
+    mapping = get_mapping(file)
+    col_type_dest = get_type(dschema, dtable)
+    pk = get_primary_key(table_name)
+    lst = list()
+    for key in mapping.keys():
+        if mapping[key]['with_comma']:
+            lst.append('"%s"' % key)
+        else:
+            lst.append(key)
+    lst.append('ROW_NUMBER() OVER(ORDER BY %s)' % pk)
+    select_str = ",".join(lst)
+
+    insert_sql, key_lst = create_insert_part(dschema, dtable, col_type_dest, mapping)
 
     while current <= end_date:
         sd = current.strftime('%Y-%m-%d 00:00:00')
         ed = current.strftime('%Y-%m-%d 23:59:59')
         print('Processing from %s to %s' % (sd, ed))
-        create_batch_insert(destination_schema, destination_table, schema, table_name, sd, ed)
+        create_batch_insert(schema, table_name, col_type_dest, select_str, mapping, insert_sql, key_lst, sd, ed)
         current += datetime.timedelta(days=1)
 
 
@@ -93,7 +98,7 @@ def main():
     parser.add_argument('-ds', help="Destination schema name", default='public')
     parser.add_argument('-dt', help="Destination schema name", default='stock_inventory_line')
     parser.add_argument('-sd', help="Start date in format 'YYYY-MM-dd'", default='2016-06-17')
-    parser.add_argument('-ed', help="End date in format 'YYYY-MM-dd'", default='2016-12-31')
+    parser.add_argument('-ed', help="End date in format 'YYYY-MM-dd'", default='2016-06-30')
     arguments = parser.parse_args()
     disable_triggers(arguments.ds, arguments.dt)
     count_down(arguments.ds, arguments.dt, arguments.s, arguments.t, arguments.sd, arguments.ed)
